65e6a07697ba3fd94431fb9a108fb50d51286d56
David Segura <david@segura.org>
2021-08-12 14:22:45 -0500

    Update Invoke-OOBEDeploy.ps1

--- OOBE/Invoke-OOBEDeploy.ps1
+++ OOBE/Invoke-OOBEDeploy.ps1
@@ -1,7 +1,6 @@
 $Params = @{
     AddNetFX3 = $true
     AddRSAT = $true
-    Autopilot = $true
     RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
     SetEdition = 'Enterprise'
     UpdateDrivers = $true
