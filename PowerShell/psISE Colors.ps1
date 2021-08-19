327fd10a7c2da0effeaac2475ff85c5d4fdc015e
David Segura <david@segura.org>
2021-08-19 09:26:52 -0500

    Create psISE Colors.ps1

---
+++ PowerShell/psISE Colors.ps1
@@ -0,0 +1,5 @@
+#Default colors if you mess it up like me
+$psISE.Options.ConsolePaneBackgroundColor = '#FF012456'
+$psISE.Options.ConsolePaneTextBackgroundColor = '#FF012456'
+$psISE.Options.ConsolePaneForegroundColor = '#FFF5F5F5'
+Clear-Host
